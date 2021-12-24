ef4beb821d30dfeb0ee9ead5afb67917e9172db4
Mario Dujić <mario.dujic@gmail.com>
2021-12-24 15:01:47 +0100

    Add custom fonts

--- app/src/main/java/com/zero/neon/common/theme/Type.kt
+++ app/src/main/java/com/zero/neon/common/theme/Type.kt
@@ -2,12 +2,22 @@ package com.zero.neon.common.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.zero.neon.R
 
-// Set of Material typography styles to start with
+val OrbitronFontFamily = FontFamily(
+    Font(R.font.orbitron_regular),
+    Font(R.font.orbitron_medium, FontWeight.Medium),
+    Font(R.font.orbitron_semibold, FontWeight.SemiBold),
+    Font(R.font.orbitron_bold, FontWeight.Bold),
+    Font(R.font.orbitron_black, FontWeight.Black),
+)
+
 val Typography = Typography(
+    defaultFontFamily = OrbitronFontFamily,
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
