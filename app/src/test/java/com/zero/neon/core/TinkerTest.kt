bb633bc8cad5ad6dc0d8e787d0c3241f63adb3c2
Mario Dujić <mario.dujic@gmail.com>
2025-11-22 08:10:28 +0100

    Update AGP, Kotlin, Mockito version

--- app/src/test/java/com/zero/neon/core/TinkerTest.kt
+++ app/src/test/java/com/zero/neon/core/TinkerTest.kt
@@ -2,27 +2,37 @@ package com.zero.neon.core
 
 import com.zero.neon.game.common.Millis
 import org.junit.Test
-import org.mockito.Mockito.*
-import java.util.*
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import java.util.UUID
 
 class TinkerTest {
+
+    fun interface Work {
+        fun run()
+    }
 
     @Test
     fun `should run work once on invoke`() {
         val fakeId = UUID.randomUUID().toString()
-        val mockWork: () -> Unit = spy {}
-        tinker(fakeId, Millis(FAKE_TIME_MILLIS), mockWork)
-        verify(mockWork)()
+        val mockWork = mock(Work::class.java)
+
+        tinker(fakeId, Millis(FAKE_TIME_MILLIS)) {
+            mockWork.run()
+        }
+
+        verify(mockWork).run()
     }
 
     @Test
     fun `should run work twice when required time has passed`() {
         val fakeId = UUID.randomUUID().toString()
-        val mockWork: () -> Unit = spy {}
-        tinker(fakeId, Millis(FAKE_TIME_MILLIS), mockWork)
+        val mockWork = mock(Work::class.java)
+        tinker(fakeId, Millis(FAKE_TIME_MILLIS)) { mockWork.run() }
         Thread.sleep(150)
-        tinker(fakeId, Millis(FAKE_TIME_MILLIS), mockWork)
-        verify(mockWork, times(2))()
+        tinker(fakeId, Millis(FAKE_TIME_MILLIS)) { mockWork.run() }
+        verify(mockWork, times(2)).run()
     }
 
     private companion object {
